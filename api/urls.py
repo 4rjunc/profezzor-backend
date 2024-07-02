fac87163d4b0d5a30374172f71e06b29d2fd3c84
H4K3R13 <arjunmundamani@gmail.com>
2024-07-03 00:10:09 +0530

    Added ninja api

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-
 from .views import test
 
-urlpatterns = [path("test/", test)]
+
+urlpatterns = [
+    path("test/", test),
+]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from ninja import NinjaAPI
+
+api = NinjaAPI()
 
 
+@api.get("/test1")
 def test(request):
     return JsonResponse({"message": "im fine!"})
