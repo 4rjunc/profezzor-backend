50ca90cf61cf158fcf9ff916d6af329cf2c4885f
H4K3R13 <arjunmundamani@gmail.com>
2024-07-04 21:59:54 +0530

    Added tables to admin panel 🛠

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import University, College, Department, Course, Student
 
-# Register your models here.
+# Register your models here
+
+tables = ["University", "College", "Department", "Course", "Student"]
+for table in tables:
+    admin.site.register(table)
